4a77bc3ab2d1e21c17ec76eaa091bf1b11902dcc
Miniruwan <miniruwan@gmail.com>
2023-07-08 20:53:18 +1200

    Removed facebook groups from config file

--- config.py
+++ config.py
@@ -5,29 +5,5 @@ config = {
     "facebook_photos_sub_folder_name": "facebook",
 
     "facebook_group_names": [
-        "Cheap Vehicle's For Sale HB NZ",
-        "Hawkes Bay Garage Sales",
-        "Napier Buy, Sell and Swap",
-        "HawkesBay Cheap Cars",
-        "Boycott Hawkes bay online garage sale",
-        "Hawkes Bay Buy/sell/swap",
-        "Napier Only swaps / sell / Buy .",
-        "BOYCOTT BOYCOTT ONLINE GARAGE SALE",
-        "Buy, sell swaps and deals Hawkes Bay....",
-        "Hustle Hawkesbay (No time wasters) NO FEES",
-        "Online garage sale Hawkes bay | NO RULES",
-        "Hawkes bay buy/sell and swap",
-        "Hastings/Napier online bargins",
-        "Online Garage Sale Hastings/Napier",
-        "Hawkes Bay Residents Buy, Swap & Sell",
-        "*NEW*Online Garage Sale Hawkes Bay ONLY",
-        "Hawkes Bay Boycott Online Garage Sale",
-        "Napier/Hastings HB Cars For Sale",
-        "Second Hand Vehicles: Hawke's Bay",
-        "HB ONLINE GARAGE SALE",
-        "Buy Cheap! Hawkes Bay",
-        "HASTINGS NEW ZEALAND ONLY BUY/SELL/SWAP",
-        "Local Sales HB",
-        "hawkesbay buy/swap/sell"
     ]
 }
