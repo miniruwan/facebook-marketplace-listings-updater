955da6024fd15530d18c8e652354478eb810811a
Miniruwan <miniruwan@gmail.com>
2024-03-11 08:29:41 +1300

    Change some columns in the sheet

--- helpers/google_sheet_helper.py
+++ helpers/google_sheet_helper.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 ACCOUNT_NAME_COLUMN = "Facebook Profile"
+UPDATE_FACEBOOK_YES_NO_COLUMN = "Update Facebook?"
 
 def get_data_from_google_sheet(sheetId):
     url = f'https://docs.google.com/spreadsheets/d/{sheetId}/export?format=csv'
     df = pd.read_csv(url, skiprows=[0,1], on_bad_lines='skip',  dtype=str) # Skip first rows    
     df = df.loc[~df['Sell Price'].notna(), :] # Unsold items    
-    df = df.loc[df[ACCOUNT_NAME_COLUMN] != 'None']
-    df = df.loc[df[ACCOUNT_NAME_COLUMN] != '']
+    df = df.loc[df[UPDATE_FACEBOOK_YES_NO_COLUMN] != 'No']
+    df = df.loc[df[UPDATE_FACEBOOK_YES_NO_COLUMN] != '']
     df.fillna('', inplace=True)
     return df.groupby(ACCOUNT_NAME_COLUMN)
